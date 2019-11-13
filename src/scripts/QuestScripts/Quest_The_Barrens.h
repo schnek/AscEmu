8878951320c27aaf32358672b5c0f923b93f0ee6
schnek <tolstikov.misha@gmail.com>
2019-11-14 00:21:09 +0300

    Update Quest_The_Barrens.h

--- src/scripts/QuestScripts/Quest_The_Barrens.h
+++ src/scripts/QuestScripts/Quest_The_Barrens.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014-2018 AscEmu Team <http://www.ascemu.org>
+Copyright (c) 2014-2019 AscEmu Team <http://www.ascemu.org>
 This file is released under the MIT license. See README-MIT for more information.
 */
 
@@ -13,22 +13,22 @@ namespace AscEmu::Scripts::Quests::TheBarrens
     // CreatureScripts
     uint32_t TheEscapeQuestIds[] =
     {
-        3439, // https://www.wowhead.com/npc=3439/wizzlecranks-shredder
+        3439, // wizzlecranks-shredder
         0
     };
 
     uint32_t FreeFromTheHoldQuestIds[] =
     {
-        3465, // https://www.wowhead.com/npc=3465/gilthares-firebough
+        3465, // gilthares-firebough
         0
     };
 
     uint32_t VerogtheDervishQuestIds[] =
     {
-        3275, // https://www.wowhead.com/npc=3275/kolkar-marauder
-        3274, // https://www.wowhead.com/npc=3274/kolkar-pack-runner
-        3397, // https://www.wowhead.com/npc=3397/kolkar-bloodcharger
-        4316, // https://www.wowhead.com/npc=4316/kolkar-packhound
+        3275, // kolkar-marauder
+        3274, // kolkar-pack-runner
+        3397, // kolkar-bloodcharger
+        4316, // kolkar-packhound
         0
     };
 } // AscEmu::Scripts::Quests::TheBarrens
