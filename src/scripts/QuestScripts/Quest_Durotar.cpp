8dc02861e9d79508d94244efaaa93c7f0a457749
schnek <tolstikov.misha@gmail.com>
2026-07-05 10:59:36 +0300

    Fix: use chopping emote state for Lazy Peon

--- src/scripts/QuestScripts/Quest_Durotar.cpp
+++ src/scripts/QuestScripts/Quest_Durotar.cpp
@@ -4,141 +4,91 @@ This file is released under the MIT license. See README-MIT for more information
 */
 
 #include "Setup.h"
-#include "Management/QuestLogEntry.hpp"
 #include "Map/Maps/MapScriptInterface.h"
-#include "Objects/GameObject.h"
 #include "Objects/Units/Players/Player.hpp"
 #include "Server/Script/CreatureAIScript.hpp"
 #include "Server/Script/QuestScript.hpp"
-#include "Utilities/Random.hpp"
-
-#include <numbers>
 
 class LazyPeon : public CreatureAIScript
 {
-    // Core Data
-    static constexpr uint32_t  QUEST_LAZY_PEONS     = 5441;
-    static constexpr uint32_t  GO_LUMBERPILE        = 175784;
-    static constexpr uint32_t  SPELL_BUFF_SLEEP     = 17743;
-    static constexpr uint32_t  SPELL_AWAKEN_PEON    = 19938;
-
-    // Tuning
-    static constexpr float SEARCH_RADIUS            = 30.0f;
-    static constexpr float RANDOM_MIN_DIST          = 5.0f;
-    static constexpr float RANDOM_MAX_DIST          = 15.0f;
-    static constexpr float CHOP_OFFSET_X            = -1.0f;
-
-    enum WaypointId : uint32_t
-    {
-        ToWoodpile = 1,
-        Random = 2
-    };
+private:
+    static constexpr uint32_t QUEST_LAZY_PEONS = 5441;
+    static constexpr uint32_t GO_LUMBERPILE = 175784;
+    static constexpr uint32_t SPELL_BUFF_SLEEP = 17743;
+    static constexpr uint32_t SPELL_AWAKEN_PEON = 19938;
+    static constexpr uint32_t WAYPOINT_LUMBERPILE = 1;
 
 public:
     static CreatureAIScript* Create(Creature* creature) { return new LazyPeon(creature); }
-    explicit LazyPeon(Creature* pCreature) : CreatureAIScript(pCreature) {}
+    explicit LazyPeon(Creature* creature) : CreatureAIScript(creature) {}
 
-    void InitOrReset() override
+    void OnLoad() override
     {
-        getCreature()->setEmoteState(EMOTE_ONESHOT_NONE);
-        // Hackfix for Updatemgr
-        addAIFunction([this](CreatureAIFunc /*pThis*/) { startSleeping(); }, DoOnceScheduler(1s));
+        isWorking = false;
+
+        addAIFunction([this](CreatureAIFunc pThis)
+        {
+            castSpellOnSelf(SPELL_BUFF_SLEEP);
+            repeatFunctionFromScheduler(pThis, 3min);
+        }, DoOnceScheduler(5s));
+    }
+
+    void AIUpdate(unsigned long /*time_passed*/) override
+    {
+        // when at woodpile do chopping emote
+        if (isWorking)
+            getCreature()->emote(EMOTE_ONESHOT_WORK_CHOPWOOD);
     }
 
     void OnReachWP(uint32_t /*type*/, uint32_t id) override
     {
-        if (id == ToWoodpile)
-        {
-            // Woodpile Reached
+        // debug sLogger.info("OnReachWP {}", id);
+
+        if (id == WAYPOINT_LUMBERPILE)
+       {
+            // debug sLogger.info("Start chopping");
+            getCreature()->setSheathType(SHEATH_STATE_MELEE);
             getCreature()->setEmoteState(EMOTE_STATE_WORK_CHOPWOOD);
-            addAIFunction([this](CreatureAIFunc pThis) {handleMoveRandom(pThis); }, DoOnceScheduler(3min));
-        }
-        else if (id == Random)
-        {
-            // Random Position Reached go Back to Sleep
-            startSleeping();
         }
     }
 
     void OnHitBySpell(uint32_t spellId, Unit* caster) override
     {
-        if (!caster || !caster->isPlayer())
+        if (spellId != SPELL_AWAKEN_PEON || !caster || !caster->isPlayer())
             return;
 
-        if (spellId != SPELL_AWAKEN_PEON)
-            return;
-
-        removeAllFunctionsFromScheduler();
-        handleWakeUp(caster);
+        handleWakeUp(caster->ToPlayer());
     }
 
-    void startSleeping()
+private:
+    void handleWakeUp(Player* player)
     {
-        castSpellOnSelf(SPELL_BUFF_SLEEP);
-        // Start working after aura expires by itself
-        addAIFunction([this](CreatureAIFunc /*PThis*/) { handleWakeUp(nullptr); }, DoOnceScheduler(2min));
-    }
-
-    void handleMoveRandom(CreatureAIFunc /*pThis*/)
-    {
-        getCreature()->setEmoteState(EMOTE_ONESHOT_NONE);
-
-        // Move to a random point near closest wood pile
-        if (GameObject* Lumberpile = findNearestGameObject(GO_LUMBERPILE, SEARCH_RADIUS))
-        {
-            LocationVector randomPosition = Lumberpile->GetPosition();
-
-            float distance  = Util::getRandomFloat(RANDOM_MIN_DIST, RANDOM_MAX_DIST);
-            float angle     = Util::getRandomFloat(0.f, std::numbers::pi_v<float>);
-
-            getCreature()->movePositionToFirstCollision(randomPosition, distance, angle);
-
-            movePoint(Random, randomPosition);
-        }
-    }
-
-    void handleWakeUp(Unit* caster)
-    {
-        // Remove Zzz aura
+        // remove Zzz aura
         _removeAura(SPELL_BUFF_SLEEP);
 
-        // Caster is nullptr when peon wakes by itself
-        if (caster != nullptr)
-        {
-            // Send chat Message
-            sendDBChatMessageByIndex(0, caster);
+        // send chat message
+        sendDBChatMessageByIndex(0, player);
 
-            // Hackfix, clear AffectedUnits for Player. Quest Increment happens trough CastedSpell in QuestMgr.
-            if (auto* questLog = caster->ToPlayer()->getQuestLogByQuestId(QUEST_LAZY_PEONS))
-                questLog->clearAffectedUnits();
-        }
-
-        // Move to closest wood pile
-        if (GameObject* Lumberpile = findNearestGameObject(GO_LUMBERPILE, SEARCH_RADIUS))
+        // quest credit for player
+        if (auto* questLog = player->getQuestLogByQuestId(QUEST_LAZY_PEONS))
         {
-            LocationVector dest
+            if (questLog->getMobCountByIndex(0) < 5)
             {
-                Lumberpile->GetPositionX() + CHOP_OFFSET_X,
-                Lumberpile->GetPositionY(),
-                Lumberpile->GetPositionZ()
-            };
-            const float orientation = faceFromTo(getCreature(), dest.x, dest.y, Lumberpile->GetPositionX(), Lumberpile->GetPositionY());
+                questLog->setMobCountForIndex(0, questLog->getMobCountByIndex(0) + 1);
+                questLog->sendUpdateAddKill(0);
+                questLog->updatePlayerFields();
+            }
+        }
 
-            movePoint(ToWoodpile, dest, true, orientation);
+        // move to closest wood pile
+        if (auto* lumberPile = findNearestGameObject(GO_LUMBERPILE, 20.0f))
+        {
+            movePoint(WAYPOINT_LUMBERPILE, lumberPile->GetPositionX() - 1.0f, lumberPile->GetPositionY(), lumberPile->GetPositionZ());
         }
     }
 
-    static constexpr float toRad(float deg) noexcept
-    {
-        return deg * std::numbers::pi_v<float> / 180.0f;
-    }
-
-    static float faceFromTo(Creature* c, float fromX, float fromY, float toX, float toY)
-    {
-        if (!c) return 0.0f;
-        const float deg = c->calcAngle(fromX, fromY, toX, toY);
-        return deg * std::numbers::pi_v<float> / 180.0f;
-    }
+private:
+    bool isWorking{ false };
 };
 
 void SetupDurotar(ScriptMgr* mgr)
